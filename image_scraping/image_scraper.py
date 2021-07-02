c8bf31481d1f41ec7b5e50c4632fc9888b0c9dcb
Ethan Zhang <y2788zha@edu.uwaterloo.ca>
2021-07-02 01:52:50 -0400

    updated image scraper

--- image_scraping/image_scraper.py
+++ image_scraping/image_scraper.py
@@ -23,18 +23,19 @@ cur.execute(
 failed = []
 skipped = []
 for id, name in cur:
-    if os.path.isfile('../hello-world/src/assets/pokemon/{0}.png'.format(id)): # if already have image, no need to get another one
+    filename = '../hello-world/src/assets/pokemon/{0}.png'.format(id)
+    if os.path.isfile(filename): # if already have image, no need to get another one
         print("image exists for pokemon {0}: {1}".format(id, name))
         skipped.append(id)
         continue
     print("getting image for pokemon {0}: {1}".format(id, name))
     try:
-        filename = 'File:{0:03d}{1}.png'.format(id, name)
-        URL = 'https://bulbapedia.bulbagarden.net/wiki/' + filename
+        alt_text = 'File:{0:03d}{1}.png'.format(id, name)
+        URL = 'https://bulbapedia.bulbagarden.net/wiki/' + alt_text
         page = requests.get(URL)
         soup = BeautifulSoup(page.content, 'html.parser')
-        img = soup.find('img', alt=filename)
-        with open('../hello-world/src/assets/pokemon/{0}.png'.format(id), 'wb') as handler:
+        img = soup.find('img', alt=alt_text)
+        with open(filename, 'wb') as handler:
             img_location = img.attrs['src']
             if not img_location.startswith("https:"):
                 img_location = "https:" + img_location
@@ -42,6 +43,8 @@ for id, name in cur:
             handler.write(response.content)
     except:
         print("failed to get image for pokemon {0}: {1}".format(id, name))
+        if os.path.exists(filename):
+            os.remove(filename)
         failed.append(id)
 
 msg = "pokemon skipped: " + ','.join(skipped) + "\npokemon failed: " + ','.join(failed)
