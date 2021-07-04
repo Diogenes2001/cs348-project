99ef6b287b376548b032d28a7ad1db58d67eedf0
Ethan Zhang <y2788zha@edu.uwaterloo.ca>
2021-07-04 06:29:07 -0400

    moved test changes to test-production.sql

--- test-sample.sql
+++ test-sample.sql
@@ -21,7 +21,7 @@ SELECT id, name, baseHp, baseSpd, baseAtk, baseDef, baseSpAtk, baseSpDef, type1,
 	ON Move.moveName = CanLearnMove.moveName
 )
 WHERE
-(type1 = 'Ground' OR type2 = 'Ground') AND Move.moveName = 'Draco Meteor'
+(type1 = 'Grass' OR type2 = 'Grass') AND accuracy >= 90 AND moveType = 'Grass'
 GROUP BY id, name, baseHp, baseSpd, baseAtk, baseDef, baseSpAtk, baseSpDef, type1, type2;
 
 ----------------------------------------------------
